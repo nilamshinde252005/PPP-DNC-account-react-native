8a22d48c81b96276260cdd917d03a2bba7473a16
Chinmay Pingale <chinmayprabhakar.pingale@vista.com>
2024-01-20 11:43:02 +0530

    Added register,splash and login screens with navigation

--- components/MyComponent.tsx
+++ components/MyComponent.tsx
@@ -1,12 +1,44 @@
 import React, {useEffect, useState} from "react";
 import { View } from "react-native";
-import { makeStyles, Text, Button, useThemeMode } from "@rneui/themed";
-import { app } from '../services/config'
-import firebase from 'firebase/app';
-import firestore from "firebase/firestore"
-import 'firebase/firestore';
-import 'firebase/auth';
+import { makeStyles, Text, Button, useThemeMode, ThemeProvider } from "@rneui/themed";
 import { SnapshotOptions } from "firebase/firestore";
+import LoginScreen from '../components/loginScreen/LoginScreen'
+import SplashScreen from '../components/splashScreen/SplashScreen'
+import RegisterScreen from '../components/registerScreen/RegisterScreen'
+import AsyncStorage from '@react-native-community/async-storage';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+import DrawerNavigationRoutes from '../components/drawerNavigationRoutes/DrawerNavigationRoutes';
+
+const Stack = createStackNavigator();
+
+const Auth = () => {
+  // Stack Navigator for Login and Sign up Screen
+  return (
+    <Stack.Navigator initialRouteName="LoginScreen">
+      <Stack.Screen
+        name="LoginScreen"
+        component={LoginScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="RegisterScreen"
+        component={RegisterScreen}
+        options={{
+          title: 'Register', //Set Header Title
+          headerStyle: {
+            backgroundColor: '#307ecc', //Set Header color
+          },
+          headerTintColor: '#fff', //Set Header text color
+          headerTitleStyle: {
+            fontWeight: 'bold', //Set Header text style
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
   const styles = useStyles();
@@ -25,36 +57,36 @@ export default function App() {
   }
 
   useEffect(() => {
-    console.log('here:', app);
-    const usersRef = app.firestore().collection('users');
-    app.auth().onAuthStateChanged(user => {
-      if (user) {
-        usersRef
-          .doc(user.uid)
-          .get()
-          .then((document) => {
-            const userData = document.data()
-            setLoading(false)
-            setUser(userData)
-          })
-          .catch((error) => {
-            setLoading(false)
-          });
-      } else {
-        setLoading(false)
-      }
-    });
+    console.log('here:');
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text h3>Start Using RNE </Text>
-      <Text style={styles.text}>
-        Open up App.tsx to start working on your app!
-      </Text>
-      <Button onPress={handleOnPress}>Switch Theme</Button>
-      
-    </View>
+    <ThemeProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="SplashScreen">
+          {/* SplashScreen which will come once for 5 Seconds */}
+          <Stack.Screen
+            name="SplashScreen"
+            component={SplashScreen}
+            // Hiding header for Splash Screen
+            options={{headerShown: false}}
+          />
+          {/* Auth Navigator: Include Login and Signup */}
+          <Stack.Screen
+            name="Auth"
+            component={Auth}
+            options={{headerShown: false}}
+          />
+          {/* Navigation Drawer as a landing page */}
+          <Stack.Screen
+            name="DrawerNavigationRoutes"
+            component={DrawerNavigationRoutes}
+            // Hiding header for Navigation Drawer
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
 
